44e5268edfbc3d019bb8d1b34998348c83f4013a
JMAC <145376972+JMacPort@users.noreply.github.com>
2024-08-07 16:29:40 -0400

    Finished foundations JS exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,13 +1,10 @@
 const repeatString = function(word, num) {
-    if (num < 0) {
-        return "ERROR"
-    }
-    let newWord = ""
+    if (num < 0) return "ERROR";
+    let newWord = "";
     for (let i = 0; i < num; i++) {
         newWord = newWord+word
     }
     return newWord
 };
 
-// Do not edit below this line
 module.exports = repeatString;
